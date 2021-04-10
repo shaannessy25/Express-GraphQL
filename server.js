301f35b5ab89d0ac3f43817edac339428f19b952
shaannessy25 <shaan.hurley25@gmail.com>
2021-04-09 21:28:08 -0700

    finished challegen 5-10

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
@@ -14,19 +13,31 @@ const schema = buildSchema(`
 		Fish
 	}
 
-	type Pets {
+	type Pet{
 		name: String!
 		species: Species!
 		age: Int!
 	}
 
+	type Time{
+		hour: String!
+		minute: String!
+		seconds: String!
+	}
+
 	type Query {
-		allPets: [Pets!]!
+		allPets: [Pet!]!
+		getPet(index: Int!): Pet
+		firstPet: Pet
+		lastPet: Pet
+		getTime: Time
+		getRandom(range: Int!): Int
 	}
 
 `);
 
 const petList = [
+	{ name: 'Shadow', species: 'Dog', age: 7 },
     { name: 'Fluffy', species: 'Dog', age: 3 },
     { name: 'Sassy', species: 'Cat', age: 4 },
     { name: 'Goldberg', species: 'Frog', age: 1 }
@@ -36,6 +47,31 @@ const petList = [
 const root = {
 	allPets: () => {
 		return petList
+	},
+	
+	getPet: ({index}) => {
+		return petList[index]
+	},
+
+	firstPet: () => {
+		return petList[0]
+	},
+
+	lastPet: () => {
+		const last = petList.length - 1
+		return petList[last]
+	},
+
+	getTime: () => {
+		return {
+			hour: new Date().getHours().toString() -  12,
+			minute: new Date().getMinutes().toString(),
+			seconds: new Date().getSeconds().toString()
+		}
+	},
+
+	getRandom: ({ range }) => {
+		return Math.floor(Math.random() * range)
 	}
 };
 
